6f890ea4e5afca921e78e3d600ffbf07ddcf2f4f
kreemer <kreemer@me.com>
2022-05-23 10:56:55 +0200

    Actually check response content

--- tests/Controller/HealthControllerTest.php
+++ tests/Controller/HealthControllerTest.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Chaos\ActuatorBundle\Tests\Controller;
 
+use Akondas\ActuatorBundle\Service\Health\Health;
+
 class HealthControllerTest extends ControllerTestCase
 {
     public function testHealthEndpoint(): void
@@ -14,5 +16,10 @@ class HealthControllerTest extends ControllerTestCase
         $response = $this->client->getResponse();
 
         self::assertEquals(200, $response->getStatusCode());
+        $response = json_decode((string) $response->getContent(), true);
+        self::assertArrayHasKey('status', $response);
+        self::assertEquals(Health::UP, $response['status']);
+        self::assertArrayHasKey('details', $response);
+        self::assertCount(0, $response['details']);
     }
 }
